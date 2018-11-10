39795734a9e991ad8feabd78b83455b0e6c0d5dc
Corei5 <Corei5@192.168.25.119>
2018-11-10 01:34:25 -0300

    Antonio Junior
    - Foi criado o Metodo Salvar no PacienteDao

--- src/java/br/apetit/Apetiti_01/enity/PacienteDTO.java
+++ src/java/br/apetit/Apetiti_01/enity/PacienteDTO.java
@@ -12,13 +12,21 @@ import org.primefaces.event.SelectEvent;
 public class PacienteDTO {
     private int Idpaciente; 
     private String Nome;
-    private int Dtnascimento; 
+    private Date Dtnascimento; 
     private String sexo;
-    private int Cpf; 
+    private String Cpf; 
+    private String Email; 
+    private String Telefone; 
     private String Endereco; 
+    private String Cep; 
     private String Complemento; 
     private String Bairro; 
+    private  String Login; 
+    private String Senha; 
+    
     private String Cidade;
+ 
+    
     private Date Agconsulta; 
     
 
@@ -29,17 +37,23 @@ public class PacienteDTO {
     }; 
     
     
-    public PacienteDTO( int Idpaciente, String Nome, int Dtnascimento, String sexo, int Cpf, String Endereco ,String Bairro,String Cidade, String Complemento ){
+    public PacienteDTO( int Idpaciente, String Nome, Date Dtnascimento, String sexo, String Cpf, String Endereco ,String Bairro,String Cidade, String Complemento, String Email , String Telefone , String Cep , String Login , String Senha){
     
         this.Idpaciente = Idpaciente;
         this.Nome = Nome; 
         this.sexo = sexo; 
         this.Endereco = Endereco; 
+        this.Email = Email; 
+        this.Telefone = Telefone; 
         this.Dtnascimento = Dtnascimento; 
         this.Bairro = Bairro; 
         this.Cidade = Cidade; 
         this.Cpf = Cpf; 
-        this.Complemento = Complemento; 
+        this.Complemento = Complemento;
+        this.Cep = Cep; 
+        this.Login = Login; 
+        this.Senha = Senha; 
+        
     };
     
     public String getNome() {
@@ -50,11 +64,11 @@ public class PacienteDTO {
         this.Nome = Nome;
     }
 
-    public int getDtnascimento() {
+    public Date getDtnascimento() {
         return Dtnascimento;
     }
 
-    public void setDtnascimento(int Dtnascimento) {
+    public void setDtnascimento(Date Dtnascimento) {
         this.Dtnascimento = Dtnascimento;
     }
 
@@ -66,11 +80,11 @@ public class PacienteDTO {
         this.sexo = sexo;
     }
 
-    public int getCpf() {
+    public String getCpf() {
         return Cpf;
     }
 
-    public void setCpf(int Cpf) {
+    public void setCpf(String Cpf) {
         this.Cpf = Cpf;
     }
 
@@ -121,16 +135,52 @@ public class PacienteDTO {
     public void setAgconsulta(Date Agconsulta) {
         this.Agconsulta = Agconsulta;
     }
- public void onDateSelect(SelectEvent event) {
+
+
+    
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    public String getCep() {
+        return Cep;
+    }
+
+    public void setCep(String Cep) {
+        this.Cep = Cep;
+    }
+
+    public String getLogin() {
+        return Login;
+    }
+
+    public void setLogin(String Login) {
+        this.Login = Login;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+    public void onDateSelect(SelectEvent event) {
         FacesContext facesContext = FacesContext.getCurrentInstance();
         SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
         facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
     }
      
-   /* public void click() {
-        PrimeFaces.current().ajax().update("form:display");
-        PrimeFaces.current().executeScript("PF('dlg').show()");
-    }
-   */
-    
 }
--- src/java/br/apetit/Apetiti_01/model/PacienteDAO.java
+++ src/java/br/apetit/Apetiti_01/model/PacienteDAO.java
@@ -5,6 +5,7 @@ package br.apetit.Apetiti_01.model;
 import br.apetit.Apetiti_01.enity.PacienteDTO;
 import br.apetit.Apetiti_01.model.DadosConexao; 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,11 +19,30 @@ public class PacienteDAO {
     {
         try{
         Connection conexao = DadosConexao.Conectar();
-        PreparedStatement ps = conexao.prepareCall("");
+        PreparedStatement ps = conexao.prepareCall("INSERT INTO TB_paciente('id_nome','nome','sexo','dtnascimento','Cpf','Email','Telefone','endereco','Cep','Bairro','Cidade','Login','Senha')");
+        ps.setString(1, paciente.getNome());
+        ps.setString(2, paciente.getSexo());
+        ps.setDate(3, new Date(paciente.getDtnascimento().getTime()));
+        ps.setString(4, paciente.getCpf());
+        ps.setString(5, paciente.getEmail());
+        ps.setString(6, paciente.getTelefone());
+        ps.setString(7, paciente.getEndereco());
+        ps.setString(8, paciente.getCep());
+        ps.setString(9,paciente.getBairro());
+        ps.setString(10, paciente.getCidade());
+        ps.setString(11, paciente.getLogin());
+        ps.setString(12, paciente.getSenha());
+        ps.execute(); 
+        DadosConexao.fechaConexaoBd();
         }catch (SQLException ex){
             Logger.getLogger(PacienteDAO.class.getName()).log(Level.SEVERE, null, ex); 
         
         }
+        
+         
+        
+        
+        
         
     }
     
